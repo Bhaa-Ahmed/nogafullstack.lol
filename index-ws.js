54664309067f885e3d7621223b6b7b9a8224955d
Bahaa <bhaaahmed188@gmail.com>
2026-01-30 16:11:33 +0200

    Add database logging

--- index-ws.js
+++ index-ws.js
@@ -1,40 +1,55 @@
-const express = require('express');
-const server = require('http').createServer();
+const express = require("express");
+const server = require("http").createServer();
 const app = express();
 const PORT = 3000;
 
-app.get('/', function(req, res) {
-  res.sendFile('index.html', {root: __dirname});
+app.get("/", function (req, res) {
+	res.sendFile("index.html", { root: __dirname });
 });
 
-server.on('request', app);
+server.on("request", app);
 
-server.listen(PORT, function () { console.log('Listening on ' + PORT); });
+server.listen(PORT, function () {
+	console.log("Listening on " + PORT);
+});
+
+process.on("SIGINT", () => {
+	wss.clients.forEach((client) => {
+		client.close();
+	});
+	server.close(() => {
+		shutdownDB();
+	});
+});
 
 /** Websocket **/
-const WebSocketServer = require('ws').Server;
+const WebSocketServer = require("ws").Server;
 
 const wss = new WebSocketServer({ server: server });
 
-wss.on('connection', function connection(ws) {
-  const numClients = wss.clients.size;
+wss.on("connection", function connection(ws) {
+	const numClients = wss.clients.size;
 
-  console.log('clients connected: ', numClients);
+	console.log("clients connected: ", numClients);
 
-  wss.broadcast(`Current visitors: ${numClients}`);
+	wss.broadcast(`Current visitors: ${numClients}`);
 
-  if (ws.readyState === ws.OPEN) {
-    ws.send('welcome!');
-  }
+	if (ws.readyState === ws.OPEN) {
+		ws.send("welcome!");
+	}
 
-  ws.on('close', function close() {
-    wss.broadcast(`Current visitors: ${wss.clients.size}`);
-    console.log('A client has disconnected');
-  });
+	db.run(`INSERT INTO visitors (count, time)
+      VALUES (${numClients}, datetime('now'))
+    `);
 
-  ws.on('error', function error() {
-    //
-  });
+	ws.on("close", function close() {
+		wss.broadcast(`Current visitors: ${wss.clients.size}`);
+		console.log("A client has disconnected");
+	});
+
+	ws.on("error", function error() {
+		//
+	});
 });
 
 /**
@@ -43,9 +58,33 @@ wss.on('connection', function connection(ws) {
  * @void
  */
 wss.broadcast = function broadcast(data) {
-  console.log('Broadcasting: ', data);
-  wss.clients.forEach(function each(client) {
-    client.send(data);
-  });
+	console.log("Broadcasting: ", data);
+	wss.clients.forEach(function each(client) {
+		client.send(data);
+	});
 };
 /** End Websocket **/
+/** begin database */
+const sqlite = require("sqlite3");
+const db = new sqlite.Database(":memory:");
+
+db.serialize(() => {
+	db.run(`
+      CREATE TABLE visitors (
+        count INTEGER,
+        time TEXT
+      )
+    `);
+});
+
+function getCounts() {
+	db.each("SELECT * FROM visitors", (err, row) => {
+		console.log(row);
+	});
+}
+
+function shutdownDB() {
+	getCounts();
+	console.log("Shutting down db");
+	db.close();
+}
