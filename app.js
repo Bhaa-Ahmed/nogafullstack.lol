06d4dbc00a49040ae59e20789ac536e52a0ff76f
Bahaa <bhaaahmed188@gmail.com>
2026-01-30 11:25:44 +0200

    d

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const http = require("http");
 
 http
 	.createServer(function (req, res) {
-		res.write("test cron job");
+		res.write("test cron job /n");
 		res.write("On the way to being a full stack engineer!");
 		res.end();
 	})
