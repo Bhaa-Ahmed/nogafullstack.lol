e3392b511eb991b62954d37dae3e72a95c716921
Bahaa <bhaaahmed188@gmail.com>
2026-01-30 11:08:57 +0200

    add console log

--- app.js
+++ app.js
@@ -1,8 +1,10 @@
 const http = require("http");
-const fs = require('fs');
 
-http.createServer(function (req, res) {
-	res.write("On the way to being a full stack engineer!");
-	res.end();
-}).listen(3000);
+http
+	.createServer(function (req, res) {
+		res.write("On the way to being a full stack engineer!");
+		res.write("test cron job");
+		res.end();
+	})
+	.listen(3000);
 console.log(`Server started on port 3000`);
