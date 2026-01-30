c262ca54dcb14cb69a293f5a0612d87cd79a6086
Bahaa <bhaaahmed188@gmail.com>
2026-01-30 10:45:42 +0200

    Add fs

--- app.js
+++ app.js
@@ -1,9 +1,8 @@
 const http = require("http");
+const fs = require('fs');
 
-http
-	.createServer(function (req, res) {
-		res.write("On the way to being a full stack engineer!");
-		res.end();
-	})
-	.listen(3000);
+http.createServer(function (req, res) {
+	res.write("On the way to being a full stack engineer!");
+	res.end();
+}).listen(3000);
 console.log(`Server started on port 3000`);
